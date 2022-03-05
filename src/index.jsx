52583cf548a4158770ecf7d4381510b26de44bfd
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-03-05 15:50:40 -0800

    Added bootstrap to project, with bootstrap scss and a custom scss file

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+// Import Bootstrap JS
+import 'bootstrap';
+
+// Import BootStrap and Custom Styles
+import './scss/application.scss';
+
 import App from './components/App';
 
 render(<App />, document.getElementById('root'));
