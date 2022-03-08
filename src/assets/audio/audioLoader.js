04d6db8464e6caa9c4c54602df036beb70b6813b
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-03-07 22:29:24 -0800

    Created single JS file to load in all audio assets for use

--- src/assets/audio/audioLoader.js
+++ src/assets/audio/audioLoader.js
@@ -1,15 +1,23 @@
 import bdClip1 from './808-bd1.mp3';
+import bdClip2 from './808-bd2.mp3';
+import clapClip1 from './808-clap.mp3';
+import hhClip1 from './808-ho1.mp3';
+import hhClip2 from './808-hh1.mp3';
+import cymbalClip1 from './808-cym1.mp3';
+import tomClip1 from './808-tom1.mp3';
+import tomClip2 from './808-tom2.mp3';
+import snareClip1 from './808-sd1.mp3';
 
 const padAudioInfo = [
-  { id: 'Q', text: 'Clap', src: bdClip1 },
-  { id: 'W', text: 'High Hat Open', src: bdClip1 },
-  { id: 'E', text: 'High Hat Closed', src: bdClip1 },
-  { id: 'A', text: 'Cymbol', src: bdClip1 },
-  { id: 'S', text: 'Tom Drum 1', src: bdClip1 },
-  { id: 'D', text: 'Tom Drum 2', src: bdClip1 },
-  { id: 'Z', text: 'Snare Drum 1', src: bdClip1 },
+  { id: 'Q', text: 'Clap', src: clapClip1 },
+  { id: 'W', text: 'High Hat Open', src: hhClip1 },
+  { id: 'E', text: 'High Hat Closed', src: hhClip2 },
+  { id: 'A', text: 'Cymbal', src: cymbalClip1 },
+  { id: 'S', text: 'Tom Drum 1', src: tomClip1 },
+  { id: 'D', text: 'Tom Drum 2', src: tomClip2 },
+  { id: 'Z', text: 'Snare Drum 1', src: snareClip1 },
   { id: 'X', text: 'Kick Drum 1', src: bdClip1 },
-  { id: 'C', text: 'Kick Drum 2', src: bdClip1 },
+  { id: 'C', text: 'Kick Drum 2', src: bdClip2 },
 ];
 
 export default padAudioInfo;
