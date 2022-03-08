2176dc509bc0e0bb0f11c8f450c3eb9fcf5ff188
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-03-07 17:58:16 -0800

    Added basic set of 9 drum audio clips

---
+++ src/assets/audio/audioLoader.js
@@ -0,0 +1,15 @@
+import bdClip1 from './808-bd1.mp3';
+
+const padAudioInfo = [
+  { id: 'Q', text: 'Clap', src: bdClip1 },
+  { id: 'W', text: 'High Hat Open', src: bdClip1 },
+  { id: 'E', text: 'High Hat Closed', src: bdClip1 },
+  { id: 'A', text: 'Cymbol', src: bdClip1 },
+  { id: 'S', text: 'Tom Drum 1', src: bdClip1 },
+  { id: 'D', text: 'Tom Drum 2', src: bdClip1 },
+  { id: 'Z', text: 'Snare Drum 1', src: bdClip1 },
+  { id: 'X', text: 'Kick Drum 1', src: bdClip1 },
+  { id: 'C', text: 'Kick Drum 2', src: bdClip1 },
+];
+
+export default padAudioInfo;
